7561d5601719c220c96fe0cfe29f230da17b905b
Fatos Morina <fatosi.morina@gmail.com>
2017-09-08 12:01:35 +0200

    Validate max number of groups per user

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,7 +1,19 @@
 class Group < ApplicationRecord
 
+  MAX_NUMBER_OF_GROUPS_PER_USER = 10
+
   belongs_to :user
   has_many :products, dependent: :destroy
+
   validates_presence_of :user, :name
 
+  validate :max_number_of_groups_per_user, on: :create
+
+private
+
+  def max_number_of_groups_per_user
+    if self.user && self.user.groups.count >= MAX_NUMBER_OF_GROUPS_PER_USER
+      errors.add :group, "You can only have ${MAX_NUMBER_OF_GROUPS_PER_USER} groups at most"
+    end
+  end
 end
--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -3,6 +3,23 @@ require 'rails_helper'
 RSpec.describe Group, type: :model do
 
   it { should validate_presence_of(:name) }
-  
+
   it { should validate_presence_of(:user) }
+
+
+  it 'number of groups per user should not be greater than the limit set' do
+    maximal_number_of_groups = Group::MAX_NUMBER_OF_GROUPS_PER_USER
+    user = build_user
+    (maximal_number_of_groups+3).times { Group.create(user: user, name: Faker::Name.name) }
+    expect(user.groups.count).to eq(10)
+  end
+
+  private
+    def build_group(user:, name:)
+      Group.new(user: user, name: name)
+    end
+
+    def build_user
+      FactoryGirl.create(:user)
+    end
 end
