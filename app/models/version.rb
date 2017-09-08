4da6801c4217e92d3123456e1a015d0ad6760123
Fatos Morina <fatosi.morina@gmail.com>
2017-09-08 18:35:24 +0200

    Remove asin from Version

--- app/models/version.rb
+++ app/models/version.rb
@@ -1,6 +1,5 @@
 class Version < ApplicationRecord
+  
   belongs_to :product
-
-  validates_presence_of :asin
   
 end
--- spec/models/version_spec.rb
+++ spec/models/version_spec.rb
@@ -2,6 +2,6 @@ require 'rails_helper'
 
 RSpec.describe Version, type: :model do
   
-  it { should validate_presence_of(:asin) }
+  it { should belong_to(:product) }
   
 end
