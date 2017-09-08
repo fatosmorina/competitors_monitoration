d4a4c584e0f18210784075e17bbda6748afe63ae
Fatos Morina <fatosi.morina@gmail.com>
2017-09-08 18:38:21 +0200

    Validate presence of product in Version

--- app/models/version.rb
+++ app/models/version.rb
@@ -2,4 +2,6 @@ class Version < ApplicationRecord
   
   belongs_to :product
   
+  validates_presence_of :product
+  
 end
--- spec/models/version_spec.rb
+++ spec/models/version_spec.rb
@@ -1,7 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Version, type: :model do
-  
+
+  it { should validate_presence_of(:product) }
+
   it { should belong_to(:product) }
-  
+
 end
